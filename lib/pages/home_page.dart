de0c279380c3b8debfe59088fc3399bb12b557ea
hamza <hmzaitahmad@gmail.com>
2024-01-20 22:19:22 +0100

    secon

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,15 +9,39 @@ class home_page extends StatefulWidget {
 }
 
 class _HomePageState extends State<home_page> {
+  var _deviceHeight;
+  var _deviceWidth;
+
+  var _selectedGame;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedGame = 0;
+  }
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Stack(
-        children: <Widget>[],
+        children: <Widget>[
+          _featureGamesWidget(),
+          // Add other widgets to the Stack as needed
+        ],
       ),
     );
   }
-}
-Widget _featureGamesWidget(){
-  return SizedBox(height: , width: , child: ,);
+
+  Widget _featureGamesWidget() {
+    return SizedBox(
+      height: _deviceHeight * 0.50,
+      width: _deviceWidth,
+      child: Container(
+        // Add content for the _featureGamesWidget here
+        child: Text("Feature Games Widget"),
+      ),
+    );
+  }
 }
